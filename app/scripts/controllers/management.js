3cbeb25f49a528c58d4f214fa396c450d8c90356
Xavier Lagunas Calpe <xlagunas@gmail.com>
2014-02-19 18:32:22 +0100

    Added calendar

---
+++ app/scripts/controllers/management.js
@@ -0,0 +1,25 @@
+(function (){
+    'use strict';
+
+    angular.module('angularClientApp')
+        .controller('ManagementCtrl', function ($scope, UserService, $log, WebsocketService) {
+            $scope.mngCategories = [
+                {category: 'ALL', name: 'all'},
+                {category: 'ACCEPTED', name: 'accepted'},
+                {category: 'REQUESTED', name: 'requested'},
+                {category: 'PENDING', name: 'pending'},
+                {category: 'BLOCKED', name: 'blocked'}
+            ];
+            $scope.mngedContacts = UserService.getUsers();
+
+            $scope.updateContact = function (_id, current, future) {
+                var msg = {_id: _id, current: current, future: future};
+                $log.info(msg);
+                WebsocketService.emit('contacts:update_list',msg, function(data){
+                    console.log(data);
+
+                });
+            };
+        });
+
+}());
